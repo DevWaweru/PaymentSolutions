5a16956ada8a5fb5c386f76f96f0aa5c463289a8
Richard Waweru <developer.waweru@gmail.com>
2018-08-11 18:39:13 +0300

    Authentication and environment setup  - Add authentication templates and urls  - Setup database, and email configurations  - Add static files

---
+++ payments/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    url('^$', views.index, name='home'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
